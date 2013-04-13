f2224515c9952a6277fb7ca79e6f03978b180097
frank <frank@privilink.com>
2013-04-13 12:56:22 +0100

    renamed PropertyClient to PropertyService

---
+++ client/src/main/java/com/hawkware/apollo/client/PropertyService.java
@@ -0,0 +1,139 @@
+package com.hawkware.apollo.client;
+
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.hawkware.apollo.client.http.DefaultHttpService;
+import com.hawkware.apollo.client.http.HttpService;
+import com.hawkware.apollo.client.http.Request;
+import com.hawkware.apollo.client.http.Response;
+import com.hawkware.apollo.client.model.Application;
+import com.hawkware.apollo.client.model.Property;
+
+public class PropertyService {
+
+    private static final String APPLICATION_URI_FORMAT = "/application/%s";
+
+    private static final String PROPERTY_URI_FORMAT = "/application/%s/property/%s";
+
+    private static final Logger logger = LoggerFactory.getLogger(PropertyService.class);
+
+    private JAXBContext jaxbContext;
+
+    private HttpService httpService;
+
+    private String application;
+
+    private String serverUrl;
+
+    private String context;
+
+    public PropertyService() {
+	try {
+	    jaxbContext = JAXBContext.newInstance(Property.class, Application.class);
+	} catch (JAXBException e) {
+	    throw new RuntimeException("could not instantiate jaxb context", e);
+	}
+	httpService = new DefaultHttpService();
+    }
+
+    public String getProperty(String name) {
+
+	Request request = new Request();
+	request.setUrl(String.format(serverUrl + PROPERTY_URI_FORMAT, application, name));
+	if (context != null && context.trim().length() > 0) {
+	    request.addHeader("Context", context);
+	}
+
+	Response response = httpService.execute(request);
+
+	String payload = response.getPayload();
+
+	logger.debug("payload=" + payload);
+
+	String value = null;
+	if (payload != null && payload.trim().length() > 0) {
+	    Property prop = (Property) convert(payload);
+	    if (prop != null) {
+		value = prop.getValue();
+	    }
+	    logger.debug("property=" + prop);
+	} else {
+	    logger.warn("property [" + name + "] could not be found for application [" + application + "]");
+	}
+	return value;
+    }
+
+    public List<Property> getProperties() {
+	List<Property> properties = new ArrayList<Property>();
+
+	Request request = new Request();
+	request.setUrl(String.format(serverUrl + APPLICATION_URI_FORMAT, application));
+	if (context != null && context.trim().length() > 0) {
+	    request.addHeader("Context", context);
+	}
+
+	Response response = httpService.execute(request);
+
+	String payload = response.getPayload();
+
+	logger.debug("payload=" + payload);
+
+	if (payload != null) {
+	    Application app = (Application) convert(payload);
+	    if (app != null) {
+		properties.addAll(app.getProperties());
+	    }
+	    logger.debug("properties=" + application);
+	}
+	return properties;
+    }
+
+    private Object convert(String payload) {
+	Object object = null;
+	try {
+	    Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+	    object = unmarshaller.unmarshal(new StringReader(payload));
+	} catch (JAXBException jbe) {
+	    throw new RuntimeException("could not unmarshall payload = [" + payload + "]");
+	}
+	return object;
+    }
+
+    public String getApplication() {
+	return application;
+    }
+
+    public void setApplication(String application) {
+	this.application = application;
+    }
+
+    public String getServerUrl() {
+	return serverUrl;
+    }
+
+    public void setServerUrl(String serverUrl) {
+	this.serverUrl = serverUrl;
+    }
+
+    public String getContext() {
+	return context;
+    }
+
+    public void setContext(String context) {
+	this.context = context;
+    }
+
+    void setHttpService(HttpService httpService) {
+	this.httpService = httpService;
+    }
+
+}
---
+++ client/src/test/java/com/hawkware/apollo/client/PropertyServiceTest.java
@@ -0,0 +1,76 @@
+package com.hawkware.apollo.client;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import javax.xml.bind.JAXBException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.hawkware.apollo.client.http.HttpService;
+import com.hawkware.apollo.client.http.Request;
+import com.hawkware.apollo.client.http.Response;
+import com.hawkware.apollo.client.model.Property;
+import com.hawkware.apollo.client.spring.ApolloPropertyPlaceholderConfigurer;
+
+public class PropertyServiceTest {
+
+    private PropertyService propertyService;
+
+    private HttpService mockHttpService;
+
+    private final String payload = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><property name=\"mongodb.url\" timeToLive=\"86400\"><value context=\"qa\">http://api.test.hawkware.com</value></property>";
+
+    @Before
+    public void setUp() throws Exception {
+	propertyService = new PropertyService();
+	mockHttpService = Mockito.mock(HttpService.class);
+	propertyService.setHttpService(mockHttpService);
+    }
+
+    @Test
+    public void testGetPropertyExists() {
+	String expected = "http://api.test.hawkware.com";
+	Response response = new Response();
+	response.setPayload(payload);
+	Mockito.when(mockHttpService.execute((Request) Mockito.anyObject())).thenReturn(response);
+
+	String actual = propertyService.getProperty("mongodb.url");
+
+	assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetPropertyNotExists() {
+	String expected = null;
+	Response response = new Response();
+	response.setPayload(null);
+	Mockito.when(mockHttpService.execute((Request) Mockito.anyObject())).thenReturn(response);
+
+	String actual = propertyService.getProperty("something");
+
+	assertEquals(expected, actual);
+    }
+
+    public static void main(String[] args) throws JAXBException {
+
+	ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-environment-config.xml");
+	ApolloPropertyPlaceholderConfigurer appc = (ApolloPropertyPlaceholderConfigurer) ctx.getBean("appc");
+
+	String test = (String) ctx.getBean("test");
+
+	PropertyService ps = new PropertyService();
+	ps.setServerUrl("http://localhost:8080/apollo");
+	ps.setApplication("sample-app");
+	ps.setContext("dev");
+	String resp = ps.getProperty("admin.email");
+	List<Property> props = ps.getProperties();
+	System.out.println(props);
+	System.out.println(resp);
+    }
+}
