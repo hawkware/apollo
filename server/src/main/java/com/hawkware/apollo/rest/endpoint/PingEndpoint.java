6733334d23baa9e6d2145ca6e6bf61276ecf22ba
frank <frank@privilink.com>
2013-04-21 20:51:43 +0100

    added ping endpoint

---
+++ server/src/main/java/com/hawkware/apollo/rest/endpoint/PingEndpoint.java
@@ -0,0 +1,17 @@
+package com.hawkware.apollo.rest.endpoint;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/ping")
+public class PingEndpoint {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response ping() {
+	return Response.ok("OK").build();
+    }
+}
